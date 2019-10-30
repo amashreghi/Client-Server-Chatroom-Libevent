ac5c8e8574c2b83c60d04936dec281e0a91d9db0
amashreghi <amashreghi@rxnetworks.com>
2019-10-30 12:12:26 -0700

    increase the buffer size

---
+++ defs.h
@@ -0,0 +1,19 @@
+//
+//  defs.h
+//  SocketProgramming
+//
+//  Created by Ali Mashreghi on 2019-07-30.
+//  Copyright © 2019 Ali Mashreghi. All rights reserved.
+//
+
+#ifndef defs_h
+#define defs_h
+
+#define BUFF_SIZE 100
+#define PORT 8080
+#define MAX_CLIENTS_NUM 20
+
+typedef struct sockaddr sockaddr;
+typedef struct sockaddr_in sockaddr_in;
+
+#endif /* defs_h */
