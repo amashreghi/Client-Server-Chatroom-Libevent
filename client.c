cf3ef5494c8cb24884c52c59f62f0c1e4545c0df
amashreghi <amashreghi@rxnetworks.com>
2019-10-30 12:13:24 -0700

    use libevent to improve the potential performance

--- client.c
+++ client.c
@@ -1,15 +1,40 @@
 #include <event2/event.h>
 #include <event2/bufferevent.h>
 #include <event2/buffer.h>
+#include <pthread.h>
 
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/tcp.h>
 
 #include <string.h>
 #include <stdlib.h>
 
-int64_t total_bytes_read = 0;
-int64_t total_messages_read = 0;
+#include "defs.h"
+
+char *client_name;
+
+void* wait_for_input(void *arg){
+	struct bufferevent *bev = (struct bufferevent *)arg;
+	
+	char buff[BUFF_SIZE];
+
+	while(1){
+		bzero(buff, sizeof(buff));
+        int n = 0;
+		
+		for(int i = 0; i < strlen(client_name); i++){
+			buff[n++] = client_name[i];
+		}
+		
+		buff[n++] = ':';
+		buff[n++] = ' ';
+		
+        while (n + 1 < BUFF_SIZE && (buff[n++] = getchar()) != '\n');
+		
+		evbuffer_add(bufferevent_get_output(bev), buff, BUFF_SIZE);
+	}
+}
 
 static void set_tcp_no_delay(evutil_socket_t fd)
 {
@@ -30,18 +55,22 @@ static void readcb(struct bufferevent *bev, void *ctx)
 {
 	/* This callback is invoked when there is data to read on bev. */
 	struct evbuffer *input = bufferevent_get_input(bev);
-	struct evbuffer *output = bufferevent_get_output(bev);
 
-	++total_messages_read;
-	total_bytes_read += evbuffer_get_length(input);
+	char *received = malloc(BUFF_SIZE);
+	evbuffer_copyout(input, received, BUFF_SIZE);
+	evbuffer_drain(input, BUFF_SIZE);
+	
+	printf(">>>>> %s\n", received);
+}
 
-	/* Copy all the data from the input buffer to the output buffer. */
-	evbuffer_add_buffer(output, input);
+static void writecb(struct bufferevent *bev, void *ctx){
+	//this will be called but it's of no use for now
 }
 
 static void eventcb(struct bufferevent *bev, short events, void *ptr)
 {
 	if (events & BEV_EVENT_CONNECTED) {
+		printf("%s was successfully connected\n", client_name);
 		evutil_socket_t fd = bufferevent_getfd(bev);
 		set_tcp_no_delay(fd);
 	} else if (events & BEV_EVENT_ERROR) {
@@ -58,18 +87,13 @@ int main(int argc, char **argv)
 	struct timeval timeout;
 	int i;
 
-	if (argc != 5) {
-		fprintf(stderr, "Usage: client <port> <blocksize> ");
-		fprintf(stderr, "<sessions> <time>\n");
-		return 1;
+	if(argc > 1){
+		client_name = argv[1];
 	}
-
-	int port = atoi(argv[1]);
-	int block_size = atoi(argv[2]);
-	int session_count = atoi(argv[3]);
-	int seconds = atoi(argv[4]);
-	timeout.tv_sec = seconds;
-	timeout.tv_usec = 0;
+	else{
+		printf("Must provide name\n");
+		exit(0);
+	}
 
 	base = event_base_new();
 	if (!base) {
@@ -77,52 +101,39 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	char* message = malloc(block_size);
-	for (i = 0; i < block_size; ++i) {
-		message[i] = i % 128;
-	}
-
 	evtimeout = evtimer_new(base, timeoutcb, base);
 	evtimer_add(evtimeout, &timeout);
 
 	memset(&sin, 0, sizeof(sin));
 	sin.sin_family = AF_INET;
-	sin.sin_addr.s_addr = htonl(0x7f000001); /* 127.0.0.1 */
-	sin.sin_port = htons(port);
+	sin.sin_addr.s_addr = inet_addr("127.0.0.1");
+	sin.sin_port = htons(PORT);
 
-	bevs = malloc(session_count * sizeof(struct bufferevent *));
-	for (i = 0; i < session_count; ++i) {
-		struct bufferevent *bev = bufferevent_socket_new(base, -1, BEV_OPT_CLOSE_ON_FREE);
+	struct bufferevent *bev = bufferevent_socket_new(base, -1, BEV_OPT_CLOSE_ON_FREE);
 
-		bufferevent_setcb(bev, readcb, NULL, eventcb, NULL);
-		bufferevent_enable(bev, EV_READ|EV_WRITE);
-		evbuffer_add(bufferevent_get_output(bev), message, block_size);
+	bufferevent_setcb(bev, readcb, writecb, eventcb, NULL);
+	bufferevent_enable(bev, EV_READ|EV_WRITE);
 
-		if (bufferevent_socket_connect(bev,
-					(struct sockaddr *)&sin, sizeof(sin)) < 0) {
-			/* Error starting connection */
-			bufferevent_free(bev);
-			puts("error connect");
-			return -1;
-		}
-		bevs[i] = bev;
+	if (bufferevent_socket_connect(bev,
+				(struct sockaddr *)&sin, sizeof(sin)) < 0) {
+		/* Error starting connection */
+		bufferevent_free(bev);
+		puts("error connect");
+		return -1;
 	}
 
+	pthread_t waiting_thread_id;
+	
+	pthread_create(&waiting_thread_id, NULL, wait_for_input, (void *)bev);
+	
 	event_base_dispatch(base);
 
-	for (i = 0; i < session_count; ++i) {
-		bufferevent_free(bevs[i]);
-	}
-	free(bevs);
+	pthread_join(waiting_thread_id, NULL);
+
+	bufferevent_free(bev);
+
 	event_free(evtimeout);
 	event_base_free(base);
-	free(message);
 
-	printf("%zd total bytes read\n", total_bytes_read);
-	printf("%zd total messages read\n", total_messages_read);
-	printf("%.3f average messages size\n",
-			(double)total_bytes_read / total_messages_read);
-	printf("%.3f MiB/s throughtput\n",
-			(double)total_bytes_read / (timeout.tv_sec * 1024 * 1024));
 	return 0;
 }
